812c1d2458a5cfba873a4e95b9cd9bb397a129f8
Afrie Irham <afrieirham.work@gmail.com>
2024-02-20 18:13:36 +0800

    draft be a referrer frontend flow

--- next.config.js
+++ next.config.js
@@ -7,7 +7,15 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nlhvirtbnvazjfvrawxz.supabase.co",
+        pathname: "/**",
+      },
+    ],
+  },
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
    *
--- src/pages/referrer-form.tsx
+++ src/pages/referrer-form.tsx
@@ -1,9 +1,18 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Check, ChevronsUpDown } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { Button } from "~/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "~/components/ui/command";
 import {
   Form,
   FormControl,
@@ -13,14 +22,13 @@ import {
   FormLabel,
   FormMessage,
 } from "~/components/ui/form";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "~/components/ui/select";
 import { Input } from "~/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
+import { cn } from "~/lib/utils";
 import { api } from "~/utils/api";
 
 function ReferrerFormPage() {
@@ -31,6 +39,7 @@ function ReferrerFormPage() {
     phone: z.string(),
     email: z.string().email(),
     jobTitle: z.string(),
+    companyId: z.string(),
   });
 
   // 1. Define your form.
@@ -41,6 +50,7 @@ function ReferrerFormPage() {
       phone: "",
       email: "",
       jobTitle: "",
+      companyId: "",
     },
   });
 
@@ -61,9 +71,93 @@ function ReferrerFormPage() {
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="w-full space-y-8"
+          className="flex w-full flex-col space-y-8"
         >
-          <h1 className="text-2xl">About You</h1>
+          <h1 className="text-2xl">About your company</h1>
+          <FormField
+            control={form.control}
+            name="companyId"
+            render={({ field }) => {
+              const selectedCompany = companies?.find(
+                (c) => c.id === field.value,
+              );
+              return (
+                <FormItem>
+                  <FormLabel>Company</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant="outline"
+                          className={cn(
+                            "w-full justify-between",
+                            !field.value && "text-muted-foreground",
+                          )}
+                        >
+                          <div className="flex items-center space-x-2">
+                            {Boolean(field.value) && (
+                              <Image
+                                width={10}
+                                height={10}
+                                src={selectedCompany?.logoUrl ?? ""}
+                                alt={`${selectedCompany?.name} logo`}
+                                className="h-6 w-6 rounded-sm border"
+                              />
+                            )}
+                            {field.value ? (
+                              <span>{selectedCompany?.name}</span>
+                            ) : (
+                              "Add company"
+                            )}
+                          </div>
+                          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-full p-2">
+                      <Command>
+                        <CommandInput placeholder="Search company..." />
+                        <CommandEmpty>No company found.</CommandEmpty>
+                        <CommandGroup className="flex h-[380px] w-[400px] flex-col space-y-2 overflow-scroll">
+                          {companies?.map((company) => (
+                            <CommandItem
+                              key={company.id}
+                              value={company.id}
+                              onSelect={() => {
+                                if (field.value === company.id)
+                                  form.setValue("companyId", "");
+                                else form.setValue("companyId", company.id);
+                              }}
+                              className="flex items-center space-x-2 rounded p-2 hover:bg-zinc-100"
+                            >
+                              <Check
+                                className={cn(
+                                  "mr-2 h-4 w-4",
+                                  company.id === field.value
+                                    ? "opacity-100"
+                                    : "opacity-0",
+                                )}
+                              />
+                              <Image
+                                width={10}
+                                height={10}
+                                src={company.logoUrl}
+                                alt={`${company.name} logo`}
+                                className="h-6 w-6 rounded-sm border"
+                              />
+                              <p>{company.name}</p>
+                            </CommandItem>
+                          ))}
+                        </CommandGroup>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <h1 className="text-2xl">About you</h1>
           <FormField
             control={form.control}
             name="name"
@@ -114,7 +208,7 @@ function ReferrerFormPage() {
             name="jobTitle"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Your job title with seniority level</FormLabel>
+                <FormLabel>Job title (with seniority level)</FormLabel>
                 <FormControl>
                   <Input placeholder="Senior Frontend Developer" {...field} />
                 </FormControl>
@@ -122,26 +216,7 @@ function ReferrerFormPage() {
               </FormItem>
             )}
           />
-          <h1 className="text-2xl">Your Company</h1>
-          <Select>
-            <SelectTrigger>
-              <SelectValue placeholder="Choose or add your company" />
-            </SelectTrigger>
-            <SelectContent className="">
-              {companies?.map((company) => (
-                <SelectItem key={company.id} value={company.id}>
-                  <div className="flex items-center space-x-2">
-                    <img
-                      src={company.logoUrl}
-                      className="h-6 w-6 rounded-sm border"
-                      alt=""
-                    />
-                    <span>{company.name}</span>
-                  </div>
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
+
           <Button type="submit">Submit</Button>
         </form>
       </Form>
