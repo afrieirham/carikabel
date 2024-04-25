726e1d36ca3c6abf93ffc228f2e0d7bec2ec0107
Afrie Irham <afrieirham.work@gmail.com>
2024-04-25 18:03:40 +0800

    update landing page, setup SEO and branding

---
+++ src/constant.ts
@@ -0,0 +1,230 @@
+export const marketingCompanies = [
+  {
+    id: "2",
+    name: "Accord Innovations",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/accord-innovations.jpg",
+  },
+  {
+    id: "3",
+    name: "AceTeam Networks",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/aceteam-networks.jpg",
+  },
+  {
+    id: "5",
+    name: "American Express",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/american-express.jpg",
+  },
+  {
+    id: "6",
+    name: "BAE Systems Digital Intelligence",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/bae-systems-digital-intelligence.jpg",
+  },
+  {
+    id: "7",
+    name: "Beam Mobility",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/beam-mobility.jpg",
+  },
+  {
+    id: "8",
+    name: "BigPay",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/bigpay.jpg",
+  },
+  {
+    id: "10",
+    name: "Cognizant Malaysia",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/cognizant-malaysia.jpg",
+  },
+  {
+    id: "11",
+    name: "CoinGecko",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/coingecko.jpg",
+  },
+  {
+    id: "13",
+    name: "Dell Technologies",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/dell-technologies.jpg",
+  },
+  {
+    id: "44",
+    name: "Deriv",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/deriv.jpg",
+  },
+  {
+    id: "14",
+    name: "Endava",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/endava.jpg",
+  },
+  {
+    id: "15",
+    name: "Etherscan",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/etherscan.jpg",
+  },
+  {
+    id: "16",
+    name: "FPT Software Malaysia",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/fpt-software-malaysia.jpg",
+  },
+  {
+    id: "17",
+    name: "Guidewire Software",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/guidewire-software.jpg",
+  },
+  {
+    id: "19",
+    name: "Hilti Group",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/hilti-group.jpg",
+  },
+  {
+    id: "20",
+    name: "INFOPRO",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/infopro.jpg",
+  },
+  {
+    id: "21",
+    name: "Ingredion Incorporated",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/ingredion-incorporated.jpg",
+  },
+  {
+    id: "22",
+    name: "Jobstreet (SEEK / SEEK Asia)",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/jobstreet-seek--seek-asia.jpg",
+  },
+  {
+    id: "23",
+    name: "Juris Technologies",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/juris-technologies.jpg",
+  },
+  {
+    id: "24",
+    name: "Maxis",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/maxis.jpg",
+  },
+  {
+    id: "25",
+    name: "Mindvalley",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/mindvalley.jpg",
+  },
+  {
+    id: "45",
+    name: "Nintex",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/nintex.jpg",
+  },
+  {
+    id: "46",
+    name: "Novartis Corporation (Malaysia)",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/novartis-corporation-malaysia.jpg",
+  },
+  {
+    id: "27",
+    name: "OCBC Bank Malaysia",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/ocbc-bank-malaysia.jpg",
+  },
+  {
+    id: "28",
+    name: "Orsted",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/orsted.jpg",
+  },
+  {
+    id: "29",
+    name: "PayNet (Payments Network Malaysia)",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/paynet-payments-network-malaysia.jpg",
+  },
+  {
+    id: "30",
+    name: "Pickles Asia",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/pickles-asia.jpg",
+  },
+  {
+    id: "31",
+    name: "QuickDesk",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/quickdesk.jpg",
+  },
+  {
+    id: "36",
+    name: "SWM Environment Sdn Bhd",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/swm-environment-sdn-bhd.jpg",
+  },
+  {
+    id: "32",
+    name: "ServiceRocket",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/servicerocket.jpg",
+  },
+  {
+    id: "33",
+    name: "Setel Ventures",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/setel-ventures.jpg",
+  },
+  {
+    id: "34",
+    name: "ShopBack",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/shopback.jpg",
+  },
+  {
+    id: "35",
+    name: "Sime Darby Property",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/sime-darby-property.jpg",
+  },
+  {
+    id: "40",
+    name: "TNG Digital Sdn Bhd",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/tng-digital-sdn-bhd.jpg",
+  },
+  {
+    id: "37",
+    name: "TechnologyOne",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/technologyone.jpg",
+  },
+  {
+    id: "39",
+    name: "TikTok (Bytedance Ltd)",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/tiktok-bytedance-ltd.jpg",
+  },
+  {
+    id: "42",
+    name: "Uber Carshare",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/uber-carshare.jpg",
+  },
+  {
+    id: "43",
+    name: "Xsolla",
+    logoUrl:
+      "https://nlhvirtbnvazjfvrawxz.supabase.co/storage/v1/object/public/carikabel/company-logos/xsolla.jpg",
+  },
+];
